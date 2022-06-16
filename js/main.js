19518bbedf162235bb051267440445e6ac63c77a
Federico Geretto <fede.geretto96@gmail.com>
2022-06-16 20:56:16 -0300

    Vendedores con un filtro

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
-function guardarDatos(){
+const vendedoresDatos = [];
+
+function guardarDatos() {
     function VendedorUSDT(nombre, apellido, edad, usdt) {
         this.nombre = nombre;
         this.apellido = apellido;
@@ -9,20 +11,24 @@ function guardarDatos(){
     let nombreGuardado = document.getElementById("nombre").value;
     let apellidoGuardado = document.getElementById("apellido").value;
     let edadGuardado = document.getElementById("edad").value;
-    let usdtGuardado = document.getElementById("usdt").value;
+    let usdtGuardado = parseInt(document.getElementById("usdt").value);
 
     // Creando NUEVO OBJETO
+    const vendedorNuevo = new VendedorUSDT(nombreGuardado, apellidoGuardado, edadGuardado, usdtGuardado)
+    vendedoresDatos.push(vendedorNuevo);
+    imprimirTabla(vendedoresDatos); // Se agrega un nuevo vendedor y se guarda en el array
 
-    vendedorNuevo = new VendedorUSDT (nombreGuardado, apellidoGuardado, edadGuardado, usdtGuardado)
-    console.log(vendedorNuevo);
-    agregarOrden();
+    // Obtener los vendedores que vendan mas 500 usts
+    const vendedoresSuperioresA500 = vendedoresDatos.filter(vendedor => { return vendedor.usdt > 500 });
+    console.log(vendedoresSuperioresA500);
 }
 
 // Creando ARRAY
+function imprimirTabla(datosTabla) {
+    const table = document.getElementById("cuerpoTabla");
+    table.innerHTML = "";
 
-let vendedoresDatos = [];
-function agregarOrden() {
-    vendedoresDatos.push(vendedorNuevo);
-    console.log(vendedoresDatos);
-    document.getElementById("tabla").innerHTML += '<tbody><td>'+vendedorNuevo.nombre+'</td><td>'+vendedorNuevo.apellido+'</td><td>'+vendedorNuevo.edad+'</td><td>'+vendedorNuevo.usdt+'</td></tbody>';
+    for (const vendedor of datosTabla) {
+        table.innerHTML += '<tbody><td>' + vendedor.nombre + '</td><td>' + vendedor.apellido + '</td><td>' + vendedor.edad + '</td><td>' + vendedor.usdt + '</td></tbody>';
+    }
 }
